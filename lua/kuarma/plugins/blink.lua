7745d7e7d5f1f4271dea5cf9a8e880483ecd5193
Kuarma <furkyuubi@gmail.com>
2025-11-30 20:59:41 +0100

    Added a theme

--- lua/kuarma/plugins/blink.lua
+++ lua/kuarma/plugins/blink.lua
@@ -38,8 +38,16 @@ return {
 				auto_show = true,
 				draw = {
 					columns = {
-						{ "label", "label_description", gap = 1 },
-						{ "kind_icon", "kind", gap = 1 },
+						{
+							"label",
+							"label_description",
+							gap = 1,
+						},
+						{
+							"kind_icon",
+							"kind",
+							gap = 1,
+						},
 					},
 				},
 				border = "single",
---
+++ lua/kuarma/plugins/style.lua
@@ -0,0 +1,16 @@
+return {
+	{
+		"j-hui/fidget.nvim",
+		event = "LspAttach",
+		opts = {},
+	},
+	{
+		"stevearc/dressing.nvim",
+		event = "VeryLazy",
+		opts = {},
+	},
+	{
+		"nvim-tree/nvim-web-devicons",
+		lazy = true,
+	},
+}
