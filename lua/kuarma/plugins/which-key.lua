163b8eb52b0ba08c3cea73be5bb84316f9657c63
Kuarma <furkyuubi@gmail.com>
2025-12-08 14:21:10 +0100

    Added groups to which-key plugin

--- lua/kuarma/plugins/which-key.lua
+++ lua/kuarma/plugins/which-key.lua
@@ -1,4 +1,15 @@
 return {
 	"folke/which-key.nvim",
 	opts = {},
+	config = function()
+		local wk = require("which-key")
+		wk.add({
+			{ "<leader>s", group = "Telescope: Search" },
+			{ "<leader>d", group = "Debug" },
+			{ "<leader>g", group = "Go ..." },
+			{ "<leader>q", group = "Quickfix: Movement" },
+			{ "<leader>S", group = "Sessions" },
+			{ "<leader>t", group = "Todo" },
+		})
+	end,
 }
